aa1008cbbe63fc4cbd23d7ad20dfe0f3b79db0f9
Isaque <isaquesouzabraga@gmail.com>
2026-05-04 14:29:51 -0300

    feat: adiciona novas rotas das páginas de auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,21 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
     },
+    {
+      path: '/cadastrar',
+      name: 'register',
+      component: () => import('@/views/RegisterView.vue'),
+    },
+    {
+      path: '/entrar',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/recuperar-senha',
+      name: 'recover-password',
+      component: () => import('@/views/RecoverPasswordView.vue'),
+    },
   ],
 })
 
