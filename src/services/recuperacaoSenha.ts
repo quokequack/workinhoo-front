c4589233dd0928b54f7db00c0fedc34be70c9fa2
Isaque <isaquesouzabraga@gmail.com>
2026-05-27 09:11:21 -0300

    feat: adiciona rota de alterar senha

--- src/services/recuperacaoSenha.ts
+++ src/services/recuperacaoSenha.ts
@@ -1,9 +1,13 @@
 import api from '@/services/api'
 
 export async function enviarCodigoRecuperacao(email: string) {
-    return api.post('api/auth/recuperar-senha/enviar-codigo', { email })
+    return api.post('/recuperar-senha/enviar-codigo', { email })
 }
 
 export async function validarCodigoRecuperacao(token: string) {
-    return api.post('api/auth/recuperar-senha/validar', { token })
+    return api.post('/recuperar-senha/validar', { token })
+}
+
+export async function alterarSenha(senha: string) {
+    return api.post('/alterar', { senha })
 }
